64a514b9feb61c85f3df6790760c08410db2ba74
손민 <80296020+thsals@users.noreply.github.com>
2021-04-17 20:26:31 +0900

    Update note_gird.dart

--- note_gird.dart
+++ note_gird.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:note/model/note_list.dart';
 
 import 'note_widget.dart';
 
-class Notesfrid extends StatelessWidget {
+class NotesGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
       padding: EdgeInsets.all(10),
       children: List.generate(
-        100,
+        notelist.length,
         (index) {
+          Notemodel note = notelist[index];
           return Padding(
             padding: EdgeInsets.all(10),
-            child: NoteWidget(),
+            child: NoteWidget(note.title, note.content, note.time),
           );
         },
       ),
