80b3f4e9991c8625f093bc72f1f4719a8f9756f4
손민 <80296020+thsals@users.noreply.github.com>
2021-04-17 20:26:44 +0900

    Update note_wiget.dart

--- note_wiget.dart
+++ note_wiget.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NoteWidget extends StatelessWidget {
+  String title;
+  String content;
+  DateTime dateTime;
+  NoteWidget(this.title, this.content, this.dateTime);
   @override
   Widget build(BuildContext context) {
+    String date = DateFormat('yyyy-MM-DD').format(dateTime);
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ListTile(
-            title: Text('title'),
-            subtitle: Text('content'),
+            title: Text(title),
+            subtitle: Text(
+              content,
+              maxLines: 5,
+              overflow: TextOverflow.ellipsis,
+              softWrap: true,
+            ),
           ),
           Expanded(child: Container()),
           Padding(
-            child: Text('yyyy-mm-dd'),
+            child: Text(date),
             padding: EdgeInsets.only(left: 15, bottom: 10),
           ),
         ],
